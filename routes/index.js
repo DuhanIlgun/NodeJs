fbdc4236d74880e9b2c3574ea557cd302a9b8b1a
Duhan Ilgün <dhnilgunnn@gmail.com>
2025-05-17 01:12:12 +0300

    Env dosyası oluşturuldu, veritabanı şemaları hazırlandı.

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,12 @@
 var express = require('express');
+const config = require('../config');
 var router = express.Router();
+
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Duhan' });
+  res.render('index', { title: 'Duhan' , config});
 });
 
 module.exports = router;
